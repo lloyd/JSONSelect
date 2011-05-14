710b7ebd3d7329eecbbc8d32be4659804f329a37
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-05-14 09:57:15 -0600

    lexing of json strings

--- src/lex.js
+++ src/lex.js
@@ -1,12 +1,13 @@
 (function() {
     var typePat = /string|boolean|null|array|object|number/;
     var psuedoClassPat = /root|first-child|last-child|only-child/;
+    var jsonStrPat = /\"(?:[^\\]|\\[^\"])*\"/;
     var lex = function (str, off) {
         if (off == undefined) off = 0;
         while (str.length > off) {
             switch(str.charCodeAt(off)) {
             // for simple 1 char tokens, we'll let them represent themselves.
-            case 0x22: case 0x23: case 0x2a: case 0x2c:
+            case 0x23: case 0x2a: case 0x2c:
             case 0x3e: case 0x7e: 
                 return [off+1, str.charAt(off)];
             // whitespace: space, nl, tab, cr, represented as the space char
@@ -22,6 +23,17 @@
                     // XXX: pseudo functions
                 }
                 throw "unrecognized psuedo class";
+            case 0x22:
+                var m;
+                if (m = jsonStrPat.exec(str.substr(off))) {
+                    try {
+                        // XXX: we need numeric token codes
+                        return [off + m[0].length, 0, JSON.parse(m[0])];
+                    } catch(e) {
+                        throw "invalid json string";
+                    }
+                } 
+                throw "unclosed json string";                
             default: 
                 var m;
                 // test for types
