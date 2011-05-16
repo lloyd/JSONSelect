3e37fdaae61663cdc359d94f7652088a6bb7acdd
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-05-16 08:24:42 -0600

    parse combinators

--- src/parse.js
+++ src/parse.js
@@ -2,25 +2,46 @@
     var lex = JSONSelect._lex;
     var toks = JSONSelect._toks;
     var parse = function (str) {
-        return parse_selector(str, 0)[1];
+        var am = undefined;
+        var a = [];
+        var off = 0;
+        var s = parse_selector(str, 0);
+        a.push(s[1]);
+        while (true) {
+            s = lex(str, off = s[0]);
+            if (s && s[1] === ' ') s = lex(str, off = s[0]);
+            if (!s) break;
+            // now we've parsed a selector, and have something else...
+            if (s[1] === ">") {
+                a.push(">");
+                off = s[0];
+            } else if (s[1] === ",") {
+                if (am == undefined) am = [ ",", a ];
+                else am.push(a);
+                a = [];
+                off = s[0];
+            }
+            var s = parse_selector(str, off);
+            a.push(s[1]);
+            off = s[0];
+        }
+        if (am) am.push(a);
+        return am ? am : a;
     };
 
     var parse_selector = function(str, off) {
         var soff = off;
         var s = { };
         var l = lex(str, off);
-        console.log("l: ", l);
         // skip space
         if (l && l[1] === " ") { soff = off = l[0]; l = lex(str, off); }
         if (l && l[1] === toks.typ) {
             s.type = l[2];
             l = lex(str, (off = l[0]));
-            console.log("l: ", l);
         } else if (l && l[1] === "*") {
             // don't bother representing the universal sel, '*' in the
             // parse tree, cause it's the default
             l = lex(str, (off = l[0]));
-            console.log("l: ", l);
         }
 
         // now support either an id or a pc
