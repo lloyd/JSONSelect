d984a4e52c4788f089fcb4cbe0f40756149eb61c
Rick Waldron <waldron.rick@gmail.com>
2011-05-23 12:34:14 -0400

    Passing all tests; Passing jshint

--- src/jsonselect.js
+++ src/jsonselect.js
@@ -5,27 +5,18 @@
 (function(exports) {
 
     function jsonParse(str) { 
-      
       try { 
-
           if(JSON && JSON.parse){
               return JSON.parse(str);
           } 
-  
           return (new Function("return " + str))();
-          
       } catch(e) { 
         te("ijs");
       }
     }
 
-    //var jp = (typeof JSON !== 'undefined' ? JSON.parse : eval);
-    //function jsonParse(s) { try { return jp(s); } catch(e) { te("ijs"); }; }
-
-    // emitted error codes.  Strip this table for an, uh, "optimized build"
-    var _es = {}; // overshadow any globals when the table is stripped
-    /** --->>> **/
-    var _es = {
+    // emitted error codes.
+    var errorCodes = {
         "ijs": "invalid json string",
         "mpc": "multiple pseudo classes (:xxx) not allowed",
         "mepf": "malformed expression in pseudo-function",
@@ -38,19 +29,21 @@
     };
     /** <<<--- **/
     // throw a full or abbreviated error message depending on the existence of the
-    // _es table
-    var te = function(ec) { throw (_es[ec] ? _es[ec] : "jsonselect error: "+ec) }
+    // errorCodes table
+    var te = function(ec) { 
+        throw new Error(errorCodes[ec] ? errorCodes[ec] : "jsonselect error: "+ec);
+    };
 
     // THE LEXER
     var toks = {
         psc: 1, // pseudo class
         psf: 2, // pseudo class function
         typ: 3, // type
-        str: 4, // string
+        str: 4 // string
     };
 
-    var pat = /^(?:([\r\n\t\ ]+)|([*.,>])|(string|boolean|null|array|object|number)|(:(?:root|first-child|last-child|only-child))|(:(?:nth-child|nth-last-child))|(:\w+)|(\"(?:[^\\]|\\[^\"])*\")|(\")|((?:[_a-zA-Z]|[^\0-\0177]|\\[^\r\n\f0-9a-fA-F])(?:[_a-zA-Z0-9-]|[^\u0000-\u0177]|(?:\\[^\r\n\f0-9a-fA-F]))*))/;
-    var exprPat = /^\s*\(\s*(?:([+-]?)([0-9]*)n\s*(?:([+-])\s*([0-9]))?|(odd|even)|([+-]?[0-9]+))\s*\)/;
+    var pat = /^(?:([\r\n\t\ ]+)|([*.,>])|(string|boolean|null|array|object|number)|(:(?:root|first-child|last-child|only-child))|(:(?:nth-child|nth-last-child))|(:\w+)|(\"(?:[^\\]|\\[^\"])*\")|(\")|((?:[_a-zA-Z]|[^\0-\0177]|\\[^\r\n\f0-9a-fA-F])(?:[_a-zA-Z0-9\-]|[^\u0000-\u0177]|(?:\\[^\r\n\f0-9a-fA-F]))*))/;
+    var exprPat = /^\s*\(\s*(?:([+\-]?)([0-9]*)n\s*(?:([+\-])\s*([0-9]))?|(odd|even)|([+\-]?[0-9]+))\s*\)/;
     var lex = function (str, off) {
         if (!off) off = 0;
         var m = pat.exec(str.substr(off));
@@ -72,7 +65,9 @@
     // THE PARSER
 
     var parse = function (str) {
-        var am = undefined, a = [], off = 0;
+        var a = [], off = 0, 
+        am;
+        
         while (true) {
             var s = parse_selector(str, off);
             a.push(s[1]);
@@ -84,7 +79,7 @@
                 a.push(">");
                 off = s[0];
             } else if (s[1] === ",") {
-                if (am == undefined) am = [ ",", a ];
+                if (am === undefined) am = [ ",", a ];
                 else am.push(a);
                 a = [];
                 off = s[0];
@@ -144,7 +139,7 @@
                     s.a = 0;
                     s.b = parseInt(m[6], 10);
                 } else {
-                    s.a = parseInt((m[1] ? m[1] : "+") + (m[2] ? m[2] : "1"),10)
+                    s.a = parseInt((m[1] ? m[1] : "+") + (m[2] ? m[2] : "1"),10);
                     s.b = m[3] ? parseInt(m[3] + m[4],10) : 0;
                 }
                 l[0] += m[0].length;
@@ -176,7 +171,7 @@
     function mn(node, sel, id, num, tot) {
         var sels = [];
         var cs = (sel[0] === '>') ? sel[1] : sel[0];
-        var m = true;
+        var m = true, mod;
         if (cs.type) m = m && (cs.type === mytypeof(node));
         if (cs.id)   m = m && (cs.id === id);
         if (m && cs.pf) {
@@ -184,11 +179,13 @@
             else num++;
             if (cs.a === 0) {
                 m = cs.b === num;
-            } else {
-                m = (!((num - cs.b) % cs.a) && ((num*cs.a + cs.b) >= 0));
+            } else {                                                    
+                mod = ((num - cs.b) % cs.a);
+
+                m = (!mod && ((num*cs.a + cs.b) >= 0));
             }
         }
-
+        
         // should we repeat this selector for descendants?
         if (sel[0] !== '>' && sel[0].pc !== ":root") sels.push(sel);
 
@@ -202,50 +199,79 @@
     }
 
     function forEach(sel, obj, fun, id, num, tot) {
-        var a = (sel[0] === ',') ? sel.slice(1) : [sel];
-        var a0 = [];
-        var call = false;
-        for (var i = 0; i < a.length; i++) {
-            var x = mn(obj, a[i], id, num, tot);
-            if (x[0]) call = true;
-            for (var j = 0; j < x[1].length; j++) a0.push(x[1][j]);
+        var a = (sel[0] === ',') ? sel.slice(1) : [sel],
+        a0 = [],
+        call = false, 
+        i = 0, j = 0, l = 0, k, x; 
+        for (i = 0; i < a.length; i++) {
+            x = mn(obj, a[i], id, num, tot);
+            if (x[0]) {
+                call = true;
+            }
+            for (j = 0; j < x[1].length; j++) {
+                a0.push(x[1][j]);
+            }
         }
         if (a0.length && typeof obj === 'object') {
-            if (a0.length >= 1) a0.unshift(",");
+            if (a0.length >= 1) {
+                a0.unshift(",");
+            }
             if (isArray(obj)) {
-                for (var i = 0; i < obj.length; i++) forEach(a0, obj[i], fun, undefined, i, obj.length);
+                for (i = 0; i < obj.length; i++) {
+                    forEach(a0, obj[i], fun, undefined, i, obj.length);
+                }
             } else {
                 // it's a shame to do this for :last-child and other
                 // properties which count from the end when we don't
                 // even know if they're present.  Also, the stream
                 // parser is going to be pissed.
-                var l = 0;
-                for (var k in obj) if (obj.hasOwnProperty(k)) l++;
-                var i = 0;
-                for (var k in obj) if (obj.hasOwnProperty(k)) forEach(a0, obj[k], fun, k, i++, l);
+                l = 0;
+                for (k in obj) {
+                    if (obj.hasOwnProperty(k)) {
+                        l++;
+                    }
+                }
+                i = 0;
+                for (k in obj) {
+                    if (obj.hasOwnProperty(k)) {
+                        forEach(a0, obj[k], fun, k, i++, l);
+                    }
+                }
             }
         }
-        if (call && fun) fun(obj);
-    };
+        if (call && fun) {
+            fun(obj);
+        }
+    }
 
     function match(sel, obj) {
         var a = [];
-        forEach(sel, obj, function(x) { a.push(x); });
+        forEach(sel, obj, function(x) { 
+            a.push(x); 
+        });
         return a;
-    };
+    }
 
     function compile(sel) {
         return {
             sel: parse(sel),
-            match: function(obj){return match(this.sel, obj)},
-            forEach: function(obj, fun) { return forEach(this.sel, obj, fun) }
+            match: function(obj){
+                return match(this.sel, obj);
+            },
+            forEach: function(obj, fun) { 
+                return forEach(this.sel, obj, fun);
+            }
         };
     }
 
     exports._lex = lex;
     exports._parse = parse;
-    exports.match = function (sel, obj) { return compile(sel).match(obj) };
-    exports.forEach = function(sel, obj, fun) { return compile(sel).forEach(obj, fun) };
+    exports.match = function (sel, obj) { 
+        return compile(sel).match(obj);
+    };
+    exports.forEach = function(sel, obj, fun) { 
+        return compile(sel).forEach(obj, fun); 
+    };
     exports.compile = compile;
 })(typeof exports === "undefined" ? (window.JSONSelect = {}) : exports);
 
