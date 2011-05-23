5c5c052565b698717fedc4702002904665a0c545
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-05-23 12:00:12 -0600

    de-emphasize build mechanism by moving it under src/

--- src/build/jshint-check.js
+++ src/build/jshint-check.js
@@ -1,6 +1,6 @@
 var JSHINT = require("./lib/jshint").JSHINT,
 	print = require("sys").print,
-	src = require("fs").readFileSync("src/jsonselect.js", "utf8");
+	src = require("fs").readFileSync("jsonselect.js", "utf8");
 
 JSHINT(src, { evil: true, forin: true, maxerr: 100 });
 
