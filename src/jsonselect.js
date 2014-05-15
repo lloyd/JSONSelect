9d88794fbc954e9014d2df69c3a2fb12529e0ba0
Daniel Boelzle [:dbo] <dbo@boelzle.org>
2014-05-15 11:32:19 +0200

    passing key and parent object

--- src/jsonselect.js
+++ src/jsonselect.js
@@ -492,7 +492,7 @@
         return [m, sels];
     }
 
-    function forEach(sel, obj, fun, id, num, tot) {
+    function forEach(sel, obj, fun, id, num, tot, parent) {
         var a = (sel[0] === ",") ? sel.slice(1) : [sel],
         a0 = [],
         call = false,
@@ -512,18 +512,18 @@
             }
             if (isArray(obj)) {
                 for (i = 0; i < obj.length; i++) {
-                    forEach(a0, obj[i], fun, undefined, i, obj.length);
+                    forEach(a0, obj[i], fun, undefined, i, obj.length, obj);
                 }
             } else {
                 for (k in obj) {
                     if (obj.hasOwnProperty(k)) {
-                        forEach(a0, obj[k], fun, k);
+                        forEach(a0, obj[k], fun, k, undefined, undefined, obj);
                     }
                 }
             }
         }
         if (call && fun) {
-            fun(obj);
+            fun(obj, id === undefined ? num : id, parent);
         }
     }
 
